d6d5524a0347403f0ba5123c24e8c98516c5a639
Alfie Reeves <reevesalfie@gmail.com>
2022-03-17 16:06:54 +0000

    camera frustum fix

--- matrix.js
+++ matrix.js
@@ -63,6 +63,7 @@ const scene = new THREE.Scene();
 
 // -- Camera -- // 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.far = 2000
 camera.lookAt(0, 0, 0);
 
 
@@ -1006,8 +1007,8 @@ oneeightyZ.addEventListener("click", () => {
 
 // -- Grid Plane -- //
 
-var size = 100;
-var step = 100;
+var size = 60;
+var step = 60;
 
 const grid = new THREE.GridHelper(size, step);
 scene.add(grid);
